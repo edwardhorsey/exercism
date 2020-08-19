8d9e5f4e19120d2a452099e0b80a2c477d74489d
Edward Horsey <edwardhorsey@me.com>
2020-08-19 10:38:42 +0100

    python grade-school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,20 @@
+from functools import reduce
+
 class School:
     def __init__(self):
-        pass
+        self.all_classes = {}
 
     def add_student(self, name, grade):
-        pass
+        self.all_classes.setdefault(grade, []).append(name)
+        self.all_classes[grade].sort()            
 
     def roster(self):
-        pass
+        grades = sorted(self.all_classes.keys())
+        return reduce(lambda x,y: x+y, [self.grade(grade) for grade in grades], [])
+
 
     def grade(self, grade_number):
-        pass
+        if grade_number in self.all_classes:
+            return self.all_classes[grade_number]
+        else:
+            return []
