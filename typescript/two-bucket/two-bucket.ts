23e6ed406ed30981746fc34d1dd26942561d5061
edwardhorsey <edwardhorsey@me.com>
2020-10-03 13:28:35 +0100

    typescript two bucket

--- typescript/two-bucket/two-bucket.ts
+++ typescript/two-bucket/two-bucket.ts
@@ -1,27 +1,67 @@
 export interface TwoBucket {
-  buckOne: number,
-  buckTwo: number,
-  goal: number,
-  starterBuck: string
+  buckOne: Bucket;
+  buckTwo: Bucket;
+  goal: number;
+  starterBuck: string;
+  otherBucket: number;
+  goalBucket: string;
 }
 
 export interface Bucket {
-  One: string,
-  Two: string
+  capacity: number;
+  amount: number;
 }
 
 export class TwoBucket {
-  constructor(buckOne, buckTwo, goal, starterBuck) {
-    this.buckOne = buckOne;
-    this.buckTwo = buckTwo;
-    this.goal  = goal;
+  constructor(
+    buckOne: number,
+    buckTwo: number,
+    goal: number,
+    starterBuck: string
+  ) {
+    this.buckOne = new Bucket(buckOne);
+    this.buckTwo = new Bucket(buckTwo);
+    this.goal = goal;
     this.starterBuck = starterBuck;
+    this.otherBucket = 0;
+    this.goalBucket = "";
   }
+  moves = () => {
+    const a: Bucket = this.starterBuck === "one" ? this.buckOne : this.buckTwo;
+    const b: Bucket = this.starterBuck === "one" ? this.buckTwo : this.buckOne;
+    let count: number = 1;
+    a.fill();
+    while (a.amount !== this.goal && b.amount !== this.goal) {
+      if (b.full) {
+        b.empty();
+      } else if (!a.amount) {
+        a.fill();
+      } else if (a.full || (a.amount && b.amount < b.capacity)) {
+        const amountToTransfer = a.amount <= b.free ? a.amount : b.free;
+        b.amount += amountToTransfer;
+        a.amount -= amountToTransfer;
+      }
+      count += 1;
+    }
+    this.goalBucket = this.buckOne.amount === this.goal ? "one" : "two";
+    this.otherBucket = a.amount === this.goal ? b.amount : a.amount;
+    return count;
+  };
 }
 
 export class Bucket {
-  static One = 'one';
-  static Two = 'two';
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.amount = 0;
+  }
+  get free() {
+    return this.capacity - this.amount;
+  }
+  get full() {
+    return this.capacity === this.amount;
+  }
+  fill = () => (this.amount += this.capacity);
+  empty = () => (this.amount = 0);
+  static One = "one";
+  static Two = "two";
 }
-
-// create classes
