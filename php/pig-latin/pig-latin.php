71b65172aad519dc032b350de04e84427f74a810
edwardhorsey <edwardhorsey@me.com>
2020-10-25 20:07:06 +0000

    php pig latin

---
+++ php/pig-latin/pig-latin.php
@@ -0,0 +1,17 @@
+<?php
+
+function pigLatin(string $word) :string
+{
+        preg_match('/yt|xr|(a|e|i|o|(?<!q)u)/', $word, $matches);
+        $firstVowel = strcspn($word, $matches[0]);
+        return substr($word, $firstVowel) . substr($word, 0, $firstVowel) . 'ay';
+}
+
+function translate(string $input) :string
+{
+    return array_reduce(
+        explode(' ', $input), function ($acc, $word) {
+            return $acc ? $acc . ' ' . pigLatin($word) : $acc . pigLatin($word);
+        }, ''
+    );
+}
